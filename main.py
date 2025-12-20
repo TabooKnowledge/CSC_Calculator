023561539d87f7e5c08b53e0154fe128d8b4ce1b
TbKnowledge <Outernetman42@gmail.com>
2025-12-20 18:46:15 -0500

    Scaling test 1

--- main.py
+++ main.py
@@ -16,6 +16,12 @@ frame.grid(row=0, column=0, sticky=tk.NSEW)
 label = tk.Label(frame, text="Chicken Salad Production Software", font=("Arial", 12))
 label.place(relx=0.5, rely=0.05, anchor="center")
 buttons = []
+base_font_size = max(8, int(h * .02))
+button_width = max(5, int(w * .01))
+button_height = max (2, int(h * .0025))
+print(base_font_size)
+print(button_width)
+print(button_height)
 
 def on_flavor_click(_flavor):
     print(f"Clicked {_flavor.name}")
@@ -29,8 +35,8 @@ for i, flavor in enumerate(prep_sheet.all_flavors):
     btn_start_y = .05 + (btn_step_y * i) % 1
 
     btn = tk.Button(frame, text=flavor.name,
-                    width=15, height=5,
-                    font=("Arial", 10),
+                    width=button_width, height=button_height,
+                    font=("Arial", base_font_size),
                     command=lambda f=flavor: on_flavor_click(f))
     btn.place(relx=btn_start_x, rely=btn_start_y, anchor="nw")
     buttons.append(btn)
