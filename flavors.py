5d2924b18b6327268af99edd3fe59f6d73273072
TbKnowledge <Outernetman42@gmail.com>
2025-12-19 21:18:42 -0500

    Initial Commit

---
+++ flavors.py
@@ -0,0 +1,141 @@
+import math
+from ingredients import ingredients_by_name as ingredients
+
+
+flavor_data_list = {
+    "CK": {
+        "Name": "Cranberry Kelly",
+        "Large Quick Par": 6,
+        "Small Quick Par": 6,
+        "Line Mix Par": 16,
+        "Ingredients": {ingredients["Cranberries"], ingredients["Almonds"]}
+    },
+    "FN": {
+        "Name": "Fancy Nancy",
+        "Large Quick Par": 13,
+        "Small Quick Par": 9,
+        "Line Mix Par": 28,
+        "Ingredients": {ingredients["Apples"], ingredients["Pecans"], ingredients["Grapes"]}
+    },
+    "LLB": {
+        "Name": "Lauryn's Lemon Basil",
+        "Large Quick Par": 3,
+        "Small Quick Par": 3,
+        "Line Mix Par": 12,
+        "Ingredients": {ingredients["Lauryn's Mix"]},
+    },
+    "FF": {
+        "Name": "Fruity France",
+        "Large Quick Par": 4,
+        "Small Quick Par": 4,
+        "Line Mix Par": 12,
+        "Ingredients": {ingredients["Pineapples"], ingredients["Pecans"], ingredients["Grapes"]},
+    },
+    "CC": {
+        "Name": "Classic Carol",
+        "Large Quick Par": 13,
+        "Small Quick Par": 9,
+        "Line Mix Par": 24,
+        "Ingredients": {ingredients["Classic"]},
+    },
+    "SS": {
+        "Name": "Sass Scotty",
+        "Large Quick Par": 4,
+        "Small Quick Par": 4,
+        "Line Mix Par": 16,
+        "Ingredients": {ingredients["Bacon"], ingredients["Cheese"], ingredients["Ranch"]},
+    },
+    "OOS": {
+        "Name": "Olivia's Old South",
+        "Large Quick Par": 4,
+        "Small Quick Par": 4,
+        "Line Mix Par": 12,
+        "Ingredients": {ingredients["Sweet Relish"], ingredients["Eggs"]},
+    },
+    "JH": {
+        "Name": "Jalapeno Holly",
+        "Large Quick Par": 4,
+        "Small Quick Par": 4,
+        "Line Mix Par": 12,
+        "Ingredients": {ingredients["Jalapenos"]},
+
+    },
+    "BB": {
+        "Name": "Buffalo Barclay",
+        "Large Quick Par": 3,
+        "Small Quick Par": 3,
+        "Line Mix Par": 16,
+        "Ingredients": {ingredients["Buffalo"]},
+    },
+    "BBQ": {
+        "Name": "Barbecue",
+        "Large Quick Par": 2,
+        "Small Quick Par": 2,
+        "Line Mix Par": 8,
+        "Ingredients": {ingredients["Barbecue"]},
+    },
+    "DC": {
+        "Name": "Dixie Chick",
+        "Large Quick Par": 3,
+        "Small Quick Par": 3,
+        "Line Mix Par": 12,
+        "Ingredients": {ingredients["Onions"]},
+    },
+    "KKL": {
+        "Name": "Kickin' Kay Lynne",
+        "Large Quick Par": 4,
+        "Small Quick Par": 4,
+        "Line Mix Par": 12,
+        "Ingredients": {ingredients["Kickin'"]},
+    }
+}
+
+
+class Flavor:
+    def __init__(self, flavor_data):
+        self.name = flavor_data["Name"]
+        self.large_quick_par = flavor_data["Large Quick Par"]
+        self.small_quick_par = flavor_data["Small Quick Par"]
+        self.line_mix_par = flavor_data["Line Mix Par"]
+        self.par_weight = 0
+        self.calculate_par_weight()
+        self.large_quick_on_hand = 0
+        self.small_quick_on_hand = 0
+        self.line_mix_on_hand = 0
+        self.large_quick_needed = 0
+        self.small_quick_needed = 0
+        self.line_mix_needed = 0
+        self.total_mix_weight = 0
+        self.ingredients = flavor_data["Ingredients"]
+        self.totaled_ingredient_weight = 0
+
+
+    def calculate_par_weight(self):
+        self.par_weight = self.large_quick_par + self.small_quick_par / 2 + self.line_mix_par
+
+
+    def calculate_needed(self):
+        self.calculate_targets()
+        self.calculate_total_mix_weight()
+
+
+    def calculate_targets(self):
+        self.large_quick_needed = max(0, self.large_quick_par - self.large_quick_on_hand)
+        self.small_quick_needed = max(0, self.small_quick_par - self.small_quick_on_hand)
+        self.line_mix_needed = max(0, self.line_mix_par - self.line_mix_on_hand)
+
+
+    def calculate_total_mix_weight(self):
+        on_hand = self.large_quick_on_hand + self.small_quick_on_hand / 2 + self.line_mix_on_hand
+        self.total_mix_weight = math.ceil(self.par_weight - on_hand)
+
+
+def create_flavor_objects(_all_flavor_data):
+    flavors = []
+    for flavor_data in _all_flavor_data.values():
+        flavors.append(Flavor(flavor_data))
+    return flavors
+
+
+all_flavors = create_flavor_objects(flavor_data_list)
+flavors_by_name = {flavor.name: flavor for flavor in all_flavors}
---
+++ ingredients.py
@@ -0,0 +1,45 @@
+
+
+ingredient_data_list = [
+    {"Name": "Cranberries",   "Weight": .28},
+    {"Name": "Almonds",       "Weight": .40},
+    {"Name": "Pecans",        "Weight": .27},
+    {"Name": "Grapes",        "Weight": .35},
+    {"Name": "Apples",        "Weight": .30},
+    {"Name": "Lauryn's Mix",  "Weight": .20},
+    {"Name": "Pineapples",    "Weight": .50},
+    {"Name": "Bacon",         "Weight": .28},
+    {"Name": "Ranch",         "Weight": .55},
+    {"Name": "Cheese",        "Weight": .23},
+    {"Name": "Sweet Relish",  "Weight": .65},
+    {"Name": "Eggs",          "Weight": .33},
+    {"Name": "Jalapenos",     "Weight": .50},
+    {"Name": "Buffalo",       "Weight": .50},
+    {"Name": "Barbecue",      "Weight": .50},
+    {"Name": "Onions",        "Weight": .40},
+    {"Name": "Classic",       "Weight": .00},
+    {"Name": "Kickin'",       "Weight": .00},
+]
+
+class Ingredient:
+    def __init__(self, ingredient_data):
+        self.name = ingredient_data["Name"]
+        self.weight = ingredient_data["Weight"]
+        self.tallied_weight = 0
+
+
+    def calculate_weight(self, total_weight):
+        self.tallied_weight = round((total_weight / 4) * self.weight, 2)
+
+
+def create_ingredient_objects(_all_ingredients_data):
+    ingredients = []
+    for ingredient_data in _all_ingredients_data:
+        new_ingredient = Ingredient(ingredient_data)
+        ingredients.append(new_ingredient)
+
+    return ingredients
+
+
+all_ingredients = create_ingredient_objects(ingredient_data_list)
+ingredients_by_name = {ingredient.name: ingredient for ingredient in all_ingredients}
