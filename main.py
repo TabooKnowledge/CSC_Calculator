21d7699779a8432dcaaa1f96fb81fe8e43e92ed2
TbKnowledge <Outernetman42@gmail.com>
2025-12-20 20:22:53 -0500

    v.003

--- main.py
+++ main.py
@@ -1,52 +1,82 @@
 import tkinter as tk
+from tkinter import font as tkfont
 from prep_sheet import prep_sheet
-from screeninfo import get_monitors
-import math
+import os
 
 
-root = tk.Tk()
-root.title("Chicken Salad Production Software")
-w = root.winfo_screenwidth()
-h = root.winfo_screenheight()
-root.geometry(f"{w}x{h}")
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
-frame = tk.Frame(root)
-frame.grid(row=0, column=0, sticky=tk.NSEW)
-label = tk.Label(frame, text="Chicken Salad Production Software", font=("Arial", 12))
-label.place(relx=0.5, rely=0.05, anchor="center")
-buttons = []
-if h > 1080:
-    base_font_size = 8
-    button_width = 7
-    button_height = 3
-else:
-    base_font_size = 15
-    button_width = 15
-    button_height = 2
-print(h)
-print(w)
-print(base_font_size)
-print(button_width)
-print(button_height)
+class TkManager:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Chicken Salad Production Software")
+        self.w = self.root.winfo_screenwidth()
+        self.h = self.root.winfo_screenheight()
+        self.root.geometry(f"{self.w}x{self.h}")
+        self.root.columnconfigure(0, weight=1)
+        self.root.rowconfigure(0, weight=1)
+        self.frame = tk.Frame(self.root)
+        self.frame.grid(row=0, column=0, sticky=tk.NSEW)
+        self.label = tk.Label(self.frame, text="Chicken Salad Production Software", font=("Arial", 12))
+        self.label.place(relx=0.5, rely=0.05, anchor="center")
+        self.buttons = []
+
+
+class UiCompiler:
+    def __init__(self):
+        self.base_font_size = 0
+        self.button_width = 0
+        self.button_height = 0
+        self.btn_step_y = .25
+        self.btn_step_x = .4
+
+    def initialize_ui(self, _tk_manager):
+        self.assign_ui_scale()
+        self.assign_dimensions(_tk_manager)
+
+    def assign_ui_scale(self):
+        if "ANDROID_ROOT" in os.environ:
+            self.base_font_size = 8
+            self.button_width = 7
+            self.button_height = 3
+            print("Running on Android")
+        elif os.name == "nt":
+            self.base_font_size = 15
+            self.button_width = 15
+            self.button_height = 2
+            print("Running on Windows")
+        print(self.base_font_size)
+        print(self.button_width)
+        print(self.button_height)
+
+    def assign_dimensions(self, _tk_manager):
+        for i, flavor in enumerate(prep_sheet.all_flavors):
+            column = (self.btn_step_y * i) // 1
+            btn_start_x = .05 + column * self.btn_step_x
+            btn_start_y = .05 + (self.btn_step_y * i) % 1
+
+            btn = tk.Button(_tk_manager.frame, text=flavor.name, wraplength=int(_tk_manager.w * .1),
+                            width=self.button_width, height=self.button_height,
+                            font=("Arial", self.base_font_size),
+                            command=lambda f=flavor: on_flavor_click(f))
+            btn.place(relx=btn_start_x, rely=btn_start_y, anchor="nw")
+            _tk_manager.buttons.append(btn)
+
+
+
+
+def fit_text_to_button(self, btn, text, max_width_px):
+    f = tkfont.Font(font=btn['font'])
+    size = f.actual()['size']
+    while f.measure(text) > max_width_px and size > 1:
+        size -= 1
+        f.configure(size=size)
+    btn.config(font=f)
+
 
 def on_flavor_click(_flavor):
     print(f"Clicked {_flavor.name}")
 
+tk_manager = TkManager()
+ui_compiler = UiCompiler()
+ui_compiler.initialize_ui(tk_manager)
 
-btn_step_y = .25
-btn_step_x = .4
-for i, flavor in enumerate(prep_sheet.all_flavors):
-    column = (btn_step_y * i) // 1
-    btn_start_x = .05 + column * btn_step_x
-    btn_start_y = .05 + (btn_step_y * i) % 1
-
-    btn = tk.Button(frame, text=flavor.name,
-                    width=button_width, height=button_height,
-                    font=("Arial", base_font_size),
-                    command=lambda f=flavor: on_flavor_click(f))
-    btn.place(relx=btn_start_x, rely=btn_start_y, anchor="nw")
-    buttons.append(btn)
-
-
-root.mainloop()
+tk_manager.root.mainloop()
