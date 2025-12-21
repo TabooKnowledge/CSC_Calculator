37febe9302f6a8a1567dd934a2a6fd02559f9d23
TbkAndroid <outernetman42@gmail.com>
2025-12-20 23:12:39 -0500

    Test4

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import tkinter as tk
-from operator import truediv
 from tkinter import font as tkfont
 from prep_sheet import prep_sheet
 import os
 
-def this_is_a_test():
-    test = True
-
-
+#Testingthisout
 all_resolution_data = {
     "android": {
         "base_font_size": 8,
