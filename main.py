44f6180ba219ddaede70f425dc6b2f23f5d66fcc
TbKnowledge <Outernetman42@gmail.com>
2025-12-21 21:47:58 -0500

    Import failed. Same Script

--- main.py
+++ main.py
@@ -214,6 +214,30 @@ def setup_ui():
         buttons.append(btn)
     return tk_manager, ui_manager, buttons
 
+
+class Coordinator:
+    def __init__(self):
+        self.tk_manager = None
+        self.ui_manager = None
+        self.button_creator = None
+        self.buttons = None
+        self.screen_width = 0
+        self.screen_height = 0
+
+    def initialize(self, attributes):
+        self.tk_manager = attributes[0]
+        self.ui_manager = attributes[1]
+        self.buttons = attributes[2]
+        self.screen_width = self.tk_manager.w
+        self.screen_height = self.tk_manager.h
+
+    def run(self):
+        self.tk_manager.root.mainloop()
+
+attributes = setup_ui()
+coordinator = Coordinator()
+coordinator.initialize(attributes)
+coordinator.run()
 """"
 tk_manager = TkManager()
 tk_manager.initialize()
